38c2a5e3cd3e6394a76ed06da534e9b4ec92acca
rggassner <rafael.gassner@gmail.com>
2025-05-10 06:32:12 -0300

    Update wrapper.sh

--- wrapper.sh
+++ wrapper.sh
@@ -7,6 +7,7 @@ mkdir -p audios
 mkdir -p images/nsfw
 mkdir -p images/sfw
 mkdir -p compressed
+mkdir -p docs
 while (test 1)
 do
 	unbuffer ./crawling2elk.py 2>&1 | grep -vE 'external/local_xla/xla/tsl/cuda/cudart_stub.cc:32|external/local_xla/xla/stream_executor/cuda/cuda_fft.cc:467|cuda_dnn.cc:8579|cuda_dnn.cc:8579|cuda_blas.cc:1407|computation_placer.cc:177|WARNING: All log messages before absl|tensorflow/core/platform/cpu_feature_guard.cc:210|To enable the following instructions: AVX2, in other operations, rebuild TensorFlow with the appropriate compiler flags.|external/local_xla/xla/stream_executor/cuda/cuda_platform.cc:51'

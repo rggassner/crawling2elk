6524f75a0ce3c36f0d22cea1f02a1229bce67a8e
rggassner <rafael.gassner@gmail.com>
2025-05-15 00:53:26 -0300

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,17 @@
 # INITIAL_URL = 'https://crawler-test.com/'
 INITIAL_URL = 'https://crawler-test.com/'
 
+# Elasticsearch connection configuration
+ELASTICSEARCH_HOST = "127.0.0.1"
+ELASTICSEARCH_PORT = 9200
+ELASTICSEARCH_USER = 'elastic'
+ELASTICSEARCH_PASSWORD = 'yourpassowrdhere'
+ELASTICSEARCH_CA_CERT_PATH = None
+ELASTICSEARCH_RANDOM_BUCKETS = 20
+MAX_ES_RETRIES = 10
+ES_RETRY_DELAY = 1
+URLS_INDEX = 'crawler'
+
 # Selenium config
 SELENIUM_WIDTH = 1920
 SELENIUM_HEIGHT = 1080
@@ -61,67 +72,56 @@ DOWNLOAD_COMPRESSEDS = False
 COMPRESSEDS_FOLDER = 'compressed'
 
 # NonSafeForWork parameters
-CATEGORIZE_NSFW=False
-NSFW_MIN_PROBABILITY=.78
+CATEGORIZE_NSFW = False
+NSFW_MIN_PROBABILITY = .78
 # Minimum number of pixels an image should have in order to be evaluated
 MIN_NSFW_RES = 64 * 64
-DOWNLOAD_NSFW=False
-NSFW_FOLDER='images/nsfw'
-DOWNLOAD_SFW=False
-SFW_FOLDER='images/sfw'
+DOWNLOAD_NSFW = False
+NSFW_FOLDER = 'images/nsfw'
+DOWNLOAD_SFW = False
+SFW_FOLDER = 'images/sfw'
 
 # This will include all directories from tree
 # might sound aggressive for some websites
-HUNT_OPEN_DIRECTORIES=True
+HUNT_OPEN_DIRECTORIES = True
 
 # Selenium-wire don't do well with http, so we launch a https
 # localhost webservice that allows the content to be embeded and
 # crawled
-EMBED_PORT="4443"
-HTTPS_EMBED='https://localhost:'+EMBED_PORT+'/embed.html?url='
+EMBED_PORT = "4443"
+HTTPS_EMBED = 'https://localhost:'+EMBED_PORT+'/embed.html?url='
 
 # How many async workers for each instance type
-MAX_FAST_WORKERS=2
-FAST_RANDOM_MIN_WAIT=0
-FAST_RANDOM_MAX_WAIT=0
+MAX_FAST_WORKERS = 2
+FAST_RANDOM_MIN_WAIT = 0
+FAST_RANDOM_MAX_WAIT = 0
 
-MAX_SCANNER_WORKERS=1
-
-# Elasticsearch connection configuration
-ELASTICSEARCH_HOST="127.0.0.1"
-ELASTICSEARCH_PORT=9200
-ELASTICSEARCH_USER='elastic'
-ELASTICSEARCH_PASSWORD='yourpasswordhere'
-ELASTICSEARCH_CA_CERT_PATH=None
-ELASTICSEARCH_RANDOM_BUCKETS=20
-MAX_ES_RETRIES=10
-ES_RETRY_DELAY=1
-URLS_INDEX='crawler'
+MAX_SCANNER_WORKERS = 1
 
 # Word extraction
-EXTRACT_WORDS=True
-WORDS_REMOVE_SPECIAL_CHARS=True
-WORDS_TO_LOWER=True
-WORDS_MIN_LEN=3
+EXTRACT_WORDS = True
+WORDS_REMOVE_SPECIAL_CHARS = True
+WORDS_TO_LOWER = True
+WORDS_MIN_LEN = 3
 # WORDS_MAX_LEN * WORDS_MAX_WORDS should be under 1 million for a default elastic search env
-WORDS_MAX_LEN=40
-WORDS_MAX_WORDS=24000
+WORDS_MAX_LEN = 40
+WORDS_MAX_WORDS = 24000
 
 # How many urls should each picking method return
-RANDOM_SITES_QUEUE=100
+RANDOM_SITES_QUEUE = 100
 
-MAX_DIR_LEVELS=7
-MAX_HOST_LEVELS=7
+MAX_DIR_LEVELS = 7
+MAX_HOST_LEVELS = 7
 
 # If we should or not save full html to the database
-EXTRACT_RAW_WEBCONTENT=True
+EXTRACT_RAW_WEBCONTENT = True
 # If we should or not save rendered text page to the database
-EXTRACT_MIN_WEBCONTENT=True
+EXTRACT_MIN_WEBCONTENT = True
 
-MAX_WEBCONTENT_SIZE=900000 #should be under 1 million for a default elastic search env
+MAX_WEBCONTENT_SIZE = 900000 #should be under 1 million for a default elastic search env
 
 # URL File should be one url per line
-URL_FILE='sampled_urls.txt'
+URL_FILE = 'sampled_urls.txt'
 
 # search words will be randomly chosen, web searched and crawled
 SEARCH_WORDS = [
@@ -129,7 +129,7 @@ SEARCH_WORDS = [
 ]
 
 # be_greedy = True - Save urls to database that might not work, since have not matched any regex.
-BE_GREEDY=False
+BE_GREEDY = False
 
 # host_regex_block_list do not crawl these domains. 
 host_regex_block_list = [
@@ -149,7 +149,7 @@ host_regex_allow_list = [r'.*']
 # A weight for every method of url picking
 METHOD_WEIGHTS = {
     "from_file":    0,
-    "web_search":   0,
+    "web_search":   1,
     "fewest_urls":  1,
     "less_visited": 1,
     "oldest":       1,
